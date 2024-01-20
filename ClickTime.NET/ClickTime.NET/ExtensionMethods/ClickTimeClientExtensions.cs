25be794a5d6e55f6e8fd83c21f10b6c4cbd05cac
matthewjackson <>
2024-01-20 00:57:14 -0600

    move files back to original project, and start work on basic creation method instead of trying to wrap every call

---
+++ ClickTime.NET/ClickTime.NET/ExtensionMethods/ClickTimeClientExtensions.cs
@@ -0,0 +1,34 @@
+using Microsoft.Kiota.Abstractions.Authentication;
+using Microsoft.Kiota.Http.HttpClientLibrary;
+using static Microsoft.Kiota.Abstractions.Authentication.ApiKeyAuthenticationProvider.KeyLocation;
+
+namespace ClickTime.NET.ExtensionMethods;
+
+public static class ClickTimeClientExtensions
+{
+    public static ClickTimeClient Create(string token, string allowedHost = "api.clicktime.com")
+    {
+        ApiKeyAuthenticationProvider auth = new(
+            $"Token {token}"
+            , "Authorization"
+            , Header
+            , allowedHost);
+
+        HttpClientRequestAdapter adapter = new(auth);
+        
+        return new ClickTimeClient(adapter);
+    }
+    
+    public static ClickTimeClient Create(string token, params string[] allowedHosts)
+    {
+        ApiKeyAuthenticationProvider auth = new(
+            $"Token {token}"
+            , "Authorization"
+            , Header
+            , allowedHosts);
+
+        HttpClientRequestAdapter adapter = new(auth);
+        
+        return new ClickTimeClient(adapter);
+    }
+}
